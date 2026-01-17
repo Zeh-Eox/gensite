7fe93caa0c1dfc33525f21bee42ad7e6fc3387c7
Zeh-Eox <arnolddevmail@gmail.com>
2026-01-17 02:02:59 +0000

    NotFound Page completed

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-black/50 h-[91.6vh] flex justify-center items-center">
+      <div className="rounded flex flex-col items-center justify-center text-sm max-md:px-4 py-20">
+        <h1 className="text-4xl md:text-5xl font-bold bg-linear-to-r from-white to-gray-500 bg-clip-text text-transparent">
+          404 Not Found
+        </h1>
+        <div className="h-px w-80 rounded bg-linear-to-r from-gray-400 to-gray-800 my-5 md:my-7"></div>
+        <p className="md:text-xl text-gray-400 max-w-lg text-center">
+          The page you are looking for does not exist or has been moved.
+        </p>
+        <button
+          onClick={() => {
+            window.history.length > 1 ? navigate(-1) : navigate("/");
+          }}
+          className="group flex items-center gap-1 bg-white hover:bg-gray-200 px-7 py-2.5 text-gray-800 rounded-full mt-10 font-medium active:scale-95 transition-all"
+        >
+          Go back
+          <svg
+            className="group-hover:translate-x-0.5 transition"
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+          >
+            <path
+              d="M4.583 11h12.833m0 0L11 4.584M17.416 11 11 17.417"
+              stroke="#1E1E1E"
+              strokeWidth="1.8"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
