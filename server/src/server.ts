84e5ea24869836189d6402adcebee5f8aa0e99ce
Zeh-Eox <arnolddevmail@gmail.com>
2026-01-23 18:50:52 +0000

    Backend v1 completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Express } from 'express';
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
+import userRouter from './routes/user.routes.js';
+import projectRouter from './routes/project.routes.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express();
+const app: Express = express();
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
     credentials: true
-}
+};
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
-app.all('/api/auth/{*any}', toNodeHandler(auth))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/', (_: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/project', projectRouter);
+
 
 export default app;
